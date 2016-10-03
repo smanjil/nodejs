94dc4c256c355d8383bc9b05548fdaa263ee34ec
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-10-03 13:02:26 +0545

    mongo CRUD

---
+++ remove.js
@@ -0,0 +1,33 @@
+/**
+ * Created by ano on 10/2/16.
+ */
+
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://localhost:27017/fruits';
+
+MongoClient.connect(url, function(err, db){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log('Connected to: ' + url);
+
+        var collection = db.collection('apples');
+
+        var doc1 = {name: 'red apples', color: 'red'};
+        var doc2 = {name: 'green apples', color: 'green'};
+
+        collection.remove({'name': 'red apples'}, function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log('%s', res);
+            }
+            db.close();
+        });
+    }
+});
